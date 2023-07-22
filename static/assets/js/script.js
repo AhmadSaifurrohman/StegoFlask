c680a9bf40f759b5213270e50ba54f2dbe524c6e
AHMAD SAIFURROHMAN <ahmad.saifurrohman03@gmail.com>
2023-07-22 17:07:57 +0700

    first commit

---
+++ static/assets/js/script.js
@@ -0,0 +1,29 @@
+function decode() {
+    const activeEncode = document.getElementById("encode")
+    activeEncode.classList.remove("active")
+    const encodeMenu = document.getElementById("encodeMenu")
+    encodeMenu.style.display = "none"
+
+    const activeDecode = document.getElementById("decode")
+    activeDecode.classList.add("active")
+    const decodeMenu = document.getElementById("decodeMenu")
+    decodeMenu.style.display = "block"
+}
+
+function encode() {
+    const activeDecode = document.getElementById("decode")
+    activeDecode.classList.remove("active")
+    const decodeMenu = document.getElementById("decodeMenu")
+    decodeMenu.style.display = "none"
+
+    const activeEncode = document.getElementById("encode")
+    activeEncode.classList.add("active")
+    const encodeMenu = document.getElementById("encodeMenu")
+    encodeMenu.style.display = "block"
+}
+const submitDecode = document.getElementById("submitDecode");
+submitDecode.addEventListener('submit', function(event){
+  event.preventDefault();
+});
+
+
